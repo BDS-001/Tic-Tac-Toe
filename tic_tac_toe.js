81d1fa1a1658aef22b22beda514c47d8e5677914
BDS <113209967+BDS-001@users.noreply.github.com>
2023-10-19 18:45:25 -0400

    added place gamepiece function

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,9 +1,11 @@
 //gameBoard module
 const gameBoard = (function() {
-    const board = ['x','o','z','q','w','e','y','u','i']
+    const board = ['','','','','','','','','']
 
     const changeBoard = function(index, val) {
-        board[index] = val
+        if (board[index] === '') {
+            board[index] = val
+        }
     }
 
     return {board, changeBoard}
@@ -19,9 +21,16 @@ function createPlayer(gamePiece) {
 
 // game module
 const game = (function () {
+    const players = [createPlayer('X'), createPlayer('O')]
+    let currentPlayer = 0
     
     //cacheDom
     const boardSections = document.querySelectorAll('.game-square')
+
+    //bind events
+    boardSections.forEach(function(boardSection) {
+        boardSection.addEventListener('click', placeGamePiece)
+    })
 
     _render()
 
@@ -29,6 +38,14 @@ const game = (function () {
     function _render() {
         for (let index = 0; index < gameBoard.board.length; index++) {
             boardSections[index].innerHTML = gameBoard.board[index]
+        }
+    }
+
+    function placeGamePiece(event) {
+        if (event.target.innerHTML === '') {
+            gameBoard.changeBoard(event.target.dataset.index, players[currentPlayer].gamePiece)
+            currentPlayer = (currentPlayer === 1) ? 0 : 1
+            _render()
         }
     }
     
